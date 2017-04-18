b2ce1d822c25ccf14316646247d8881135471d33
balintvecsey <balint.vecsey@gmail.com>
2017-04-18 12:39:43 +0200

    add Testcases and modify oddAvarege() to work with list wihtout elements

--- src/OddAvg/OddAvg.java
+++ src/OddAvg/OddAvg.java
@@ -1,16 +1,19 @@
+package OddAvg;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class OddAvg {
+
   public static void main(String[] args) {
 
-    System.out.println(oddAverage(new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 7))));
   }
 
-  public static int oddAverage(ArrayList<Integer> numbers) {
+  public int oddAverage(ArrayList<Integer> numbers) {
     List<Integer> oddNumbers = new ArrayList<>();
     int sum = 0;
+    int avg = 0;
     for (int i = 0; i < numbers.size(); i++) {
       if (numbers.get(i) % 2 != 0) {
         oddNumbers.add(numbers.get(i));
@@ -21,7 +24,11 @@ public class OddAvg {
       sum += oddNumbers.get(i);
     }
 
-    int avg = sum / oddNumbers.size();
+    if (oddNumbers.size() > 0) {
+      avg = sum / oddNumbers.size();
+    } else {
+      return avg;
+    }
     return avg;
   }
 }
--- src/OddAvg/OddAvgTest.java
+++ src/OddAvg/OddAvgTest.java
@@ -1,18 +1,35 @@
+package OddAvg;
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.junit.Test;
 
 /**
  * Created by Bálint on 2017. 04. 18..
  */
 public class OddAvgTest {
 
-  @org.junit.Test
-  public void oddAverage(ArrayList<Integer> numbers) {
+  @Test
+  public void TestWithEvenAndOddNumbers() {
     ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 7));
-    assertEquals(4, oddAverage(numbers));
+    OddAvg oddAvg = new OddAvg();
+    assertEquals(4, oddAvg.oddAverage(numbers));
   }
 
+  @Test
+  public void TestWithOnlyOneNumber() {
+    ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1));
+    OddAvg oddAvg = new OddAvg();
+    assertEquals(1, oddAvg.oddAverage(numbers));
+  }
+
+  @Test
+  public void TestWithEmptyList() {
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    OddAvg oddAvg = new OddAvg();
+    assertEquals(0, oddAvg.oddAverage(numbers));
+  }
 }
