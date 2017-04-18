ae0b3ef577d56d19551250ac5971fa6f53cb0877
balintvecsey <balint.vecsey@gmail.com>
2017-04-18 12:01:13 +0200

    initial commit for BlackJack task

--- src/BlackJack/Deck.java
+++ src/BlackJack/Deck.java
@@ -1,8 +1,19 @@
 package BlackJack;
 
+import java.util.List;
+
 /**
  * Created by Bálint on 2017. 04. 18..
  */
 public class Deck {
+  List<Card> deck;
+
+  public Deck(int numberOfCard){
+
+  }
+
+  public void draw(){}
+
+  public void  shuffle(){}
 
 }
