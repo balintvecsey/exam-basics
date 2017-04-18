6ff482cb95b2baabc871daf9a2865a1d4299545d
balintvecsey <balint.vecsey@gmail.com>
2017-04-18 13:00:05 +0200

    everything is working now

---
+++ src/Copy/ArgumentHandler.java
@@ -0,0 +1,8 @@
+package Copy;
+
+/**
+ * Created by Bálint on 2017. 04. 18..
+ */
+public class ArgumentHandler {
+
+}
--- src/Copy/Copy.java
+++ src/Copy/Copy.java
@@ -1,3 +1,5 @@
+package Copy;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,14 +15,6 @@ public class Copy {
   }
 
   public static void main(String[] args) {
-    // This should be the basic replica of the 'cp' command
-    // If ran from the command line without arguments
-    // It should print out the usage:
-    // copy [source] [destination]
-    // When just one argument is provided print out
-    // No destination provided
-    // When both arguments provided and the source is a file
-    // Read all contents from it and write it to the destination
     Copy myCopy = new Copy(args);
 
     myCopy.printUsage();
@@ -47,7 +41,7 @@ public class Copy {
       List<String> lines = Files.readAllLines(pathOriginal);
       Files.write(pathCopied, lines);
     } catch (IOException e) {
-      e.printStackTrace();
+      System.out.println("Error: " + e.getClass());
     }
   }
 }
